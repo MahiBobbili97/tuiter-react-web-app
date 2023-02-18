9e1fa350e6fc98dc7c81bafb18118a7df7548343
MahiBobbili97 <bobbilimahisai@gmail.com>
2023-02-17 21:38:44 -0500

    Tuit responsive

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -4,17 +4,19 @@ import PostSummaryList from "../PostSummaryList/index.js";
 function exploreComponent() {
    $('#wd-explore').append(`
 
-  <div class="row mt-2">
-   <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-    ${NavigationSidebar('explore')}
-   </div>
-   <div class="col-10 col-lg-7 col-xl-6  text-white">
-     ${PostSummaryList()}
-   </div>
-   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 text-white">
-    ${WhoToFollowList()}
-   </div>
-  </div>
+<div class="container">
+    <div class="row">
+        <div class="col-xxl-2 col-xl-2 col-lg-1 col-md-2 col-sm-2">
+			${NavigationSidebar('explore')}
+        </div>
+		<div class="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10 text-white">
+			${PostSummaryList()}
+		</div>
+		<div class="col-xxl-4 col-xl-4 col-lg-4 d-none d-lg-inline-block d-xl-inline-block d-xxl-inline-block text-white">
+			${WhoToFollowList()}
+		</div>
+	</div>
+</div>
    `);
 }
 $(exploreComponent);
