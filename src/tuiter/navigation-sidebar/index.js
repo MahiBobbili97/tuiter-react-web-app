91045e796bcd647e50aef9d27f67a9f9f5974f8c
MahiBobbili97 <bobbilimahisai@gmail.com>
2023-03-05 10:08:16 -0500

    Update index.js

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,36 +7,36 @@ const NavigationSidebar = () => {
  const active = paths[2];
   return (
     <div className="list-group">
-         <a className="list-group-item"><i class="bi bi-twitter"></i> Tuiter</a>
+         <a className="list-group-item"><i class="bi bi-twitter"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Tuiter</span></a>
          <Link to="/tuiter/home" className={`list-group-item
                         ${active === 'home'?'active':''}`}>
-           <i class="bi bi-house-door-fill"></i> Home
+           <i class="bi bi-house-door-fill"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Home</span>
          </Link>
           <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
-                <i class="bi bi-hash"></i> Explore
+                <i class="bi bi-hash"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Explore</span>
               </Link>
               <Link to="/" className="list-group-item">
-                <i class="bi bi-app"></i> Labs
+                <i class="bi bi-app"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Labs</span>
               </Link>
          <a className={`list-group-item
                         ${active === 'messages'?'active':''}`}>
-           <i class="bi bi-chat"></i> Messages
+           <i class="bi bi-chat"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Messages</span>
          </a>
          <a className={`list-group-item
                         ${active === 'bookmarks'?'active':''}`}>
-           <i class="bi bi-bookmark"></i> Bookmarks
+           <i class="bi bi-bookmark"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Bookmarks</span>
          </a>
          <a className={`list-group-item
                         ${active === 'lists'?'active':''}`}>
-           <i class="bi bi-list-ul"></i> Lists
+           <i class="bi bi-list-ul"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Lists</span>
          </a>
         <a className={`list-group-item
                             ${active === 'profile'?'active':''}`}>
-               <i class="bi bi-person-circle"></i> Profile
+               <i class="bi bi-person-circle"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">Profile</span>
              </a>
              <a className={`list-group-item
                             ${active === 'more'?'active':''}`}>
-               <i class="bi bi-three-dots"></i> More
+               <i class="bi bi-three-dots"></i><span class="d-none d-xxl-inline-block d-xl-inline-block d-lg-none ps-2">More</span>
              </a>
            </div>
   );
