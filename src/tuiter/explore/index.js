281c7f31cb9ca86cd796c5b490f8f6e383f60062
MahiBobbili97 <bobbilimahisai@gmail.com>
2023-03-05 09:49:09 -0500

    Update index.js

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -13,7 +13,7 @@ const ExploreComponent = () => {
        <div className="col-11 position-relative">
          <input placeholder="Search Tuiter"
                 className="form-control rounded-pill ps-5"/>
-         <i className="bi bi-search position-absolute wd-nudge-up"></i>
+         <i className="bi bi-search position-absolute wd-nudge-up" style={{color: "gray"}}></i>
        </div>
        <div className="col-1">
          <i className="wd-bottom-4 text-primary float-end bi bi-gear-fill fs-2 position-relative"></i>
