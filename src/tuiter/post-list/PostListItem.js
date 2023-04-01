1289d5184a9554a106685d3b8ae5708725a434eb
MahiBobbili97 <bobbilimahisai@gmail.com>
2023-04-01 14:00:14 -0400

    Update PostListItem.js

--- src/tuiter/post-list/PostListItem.js
+++ src/tuiter/post-list/PostListItem.js
@@ -41,93 +41,33 @@ const deleteTuitHandler = (id) => {
   dispatch(deleteTuitThunk(id));
 }
 
-if (post.liked) {
-    return(
-        <>
-         <li className="list-group-item">
 
-         <br/>
-                   <div class="row" >
+        return(
+            <>
+             <li className="list-group-item">
 
-                    <div class="col-1 ps-0 ">
-                         <img class="float-left float-start rounded-circle" src={`/images/${post.image}`} height = "45px" width="45px" />
-                    </div>
-                    <div class="col-11">
-                       <div class=" fw-bold">{post.userName} <i class="bi bi-check-circle-fill" style={{color: '#00aaff'}}></i> <span class="text-muted">{post.handle}<i class="bi bi-dot"></i>{post.time} </span> <i className="bi bi-x-lg float-end"
-                                                                                                                                                                                                                               onClick={() => deleteTuitHandler(post._id)}></i></div>
-                       <div class="">{post.tuit}</div>
+             <br/>
+                       <div class="row" >
 
-
-
-                         <ul class="nav   align-items-center nav-justified">
-                         <li class="nav-item">
-                           <a class="nav-link text-muted" href="#"><i class="bi bi-chat"></i>  {post.replies}</a>
-                         </li>
-                         <li class="nav-item">
-                           <a class="nav-link text-muted" href="#"><i class="bi bi-arrow-repeat"></i>  {post.retuits}</a>
-                         </li>
-                         <li class="nav-item">
-
-                           <a class="nav-link text-muted" href="#" onClick={incrementNum}><i style={{color: 'red'}} class="bi-heart-fill" id={post._id} ></i> {num}</a>
-
-
-                         </li>
-                         <li class="nav-item">
-                           <a class="nav-link text-muted " href="#"><i class="bi bi-share"></i> </a>
-                         </li>
-
-
-                    </ul>
-                    </div>
-                   </div>
-
-                 </li >
-                 </>
-        );
-}
-else
-    {return(
-             <>
-              <li className="list-group-item">
-
-              <br/>
-                        <div class="row" >
-
-                         <div class="col-1 ps-0 ">
-                              <img class="float-left float-start rounded-circle" src={`/images/${post.image}`} height = "45px" width="45px" />
-                         </div>
-                         <div class="col-11">
-                            <div class=" fw-bold">{post.userName} <i class="bi bi-check-circle-fill" style={{color: '#00aaff'}}></i> <span class="text-muted">{post.handle}<i class="bi bi-dot"></i>{post.time} </span> <i className="bi bi-x-lg float-end"
-                                                                                                                                                                                                                                    onClick={() => deleteTuitHandler(post._id)}></i></div>
-                            <div class="">{post.tuit}</div>
-
-
-
-                              <ul class="nav   align-items-center nav-justified">
-                              <li class="nav-item">
-                                <a class="nav-link text-muted" href="#"><i class="bi bi-chat"></i>  {post.replies}</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link text-muted" href="#"><i class="bi bi-arrow-repeat"></i>  {post.retuits}</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link text-muted" href="#" onClick={incrementNum}><i style={{color: 'gray'}} class="bi-heart-fill" id={post._id} ></i> {num}</a>
-
-                              </li>
-
-                              <li class="nav-item">
-                                <a class="nav-link text-muted " href="#"><i class="bi bi-share"></i> </a>
-                              </li>
-
-
-                         </ul>
-                         </div>
+                        <div class="col-1 ps-0 ">
+                             <img class="float-left float-start rounded-circle" src={`../images/${tuit.image}`} height = "45px" width="45px" />
                         </div>
+                        <div class="col-11">
+                           <div class=" fw-bold">{tuit.username} <i class="bi bi-check-circle-fill" style={{color: '#00aaff'}}></i> <span class="text-muted">{tuit.handle}<i class="bi bi-dot"></i>{tuit.time} </span> <i className="bi bi-x-lg float-end"
+                                                                                                                                                                                                                                   onClick={() => deleteTuitHandler(tuit._id)}></i></div>
+                           <div class="">{tuit.tuit}</div>
 
-                      </li >
-                      </>
-             );
-    }
+
+
+                             <TuitStats tuit={tuit}/>
+                        </div>
+                       </div>
+
+                     </li >
+                     </>
+            );
+
+
    };
    function LikeButton() {
      const [isLiked, setIsLiked] = useState(false);
