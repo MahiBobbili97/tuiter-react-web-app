1b93e1a78071501370b1b0c129894483895dc83d
MahiBobbili97 <bobbilimahisai@gmail.com>
2023-04-01 13:34:03 -0400

    Update tuits-service.js

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const LOCAL_TUITS_API = "http://localhost:4000/api";
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API1 = `${API_BASE===undefined?LOCAL_TUITS_API:API_BASE}/tuits`;
-const TUITS_API = 'https://tuiter-node-server-app-cs1234-sp23.onrender.com/api/tuits';
+const TUITS_API = `${API_BASE===undefined?LOCAL_TUITS_API:API_BASE}/tuits`;
+
 
 export const findTuits = async () => {
  const response = await axios.get(TUITS_API);
